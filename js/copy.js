7236998d7ba8f129696881e0d4d7dd6087c77bdd
Arvind Ramsankar <rarvind1999@gmail.com>
2020-04-10 01:58:41 +0530

    new blog v2.0.0

--- js/copy.js
+++
@@ -1,6 +0,0 @@
-function Copy() {
-    var url = document.getElementById('url').getAttribute('href');
-    var link = $('<input id="ass">').val(url).appendTo('body').select()
-    document.execCommand('copy')
-    $('#ass').remove();
-  } 
